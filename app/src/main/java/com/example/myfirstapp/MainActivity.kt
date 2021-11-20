779ed375f0669f0bc731fdc5ab82efbfc87fc535
podokiller <yeonu9815@gmail.com>
2021-11-21 01:53:58 +0900

    게임설명창 이동

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.myfirstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var ruleButton:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var intent = Intent(this, RuleActivity::class.java)
+        ruleButton=findViewById(R.id.Rule)
+        ruleButton.setOnClickListener {
+            startActivity(intent)
+
+        }
     }
 }
